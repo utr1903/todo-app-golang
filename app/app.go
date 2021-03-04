35c98850f4e7b6364a0b1c306ae04231dbb8f31a
ugurturkarslan <ugurturkarslan@gmail.com>
2021-03-04 15:27:37 +0100

    CORS error from UI fixed

--- app/app.go
+++ app/app.go
@@ -47,36 +47,60 @@ func initUserController(a *App) {
 	b := &controllers.Controller{Db: a.Db}
 	c := &usersmodule.UsersController{Base: b}
 
-	a.Router.HandleFunc("/users/get", c.GetUsers).Methods("GET")
-	a.Router.HandleFunc("/users/create", c.CreateUser).Methods("POST")
-	a.Router.HandleFunc("/user/get", c.GetUser).Methods("POST")
-	a.Router.HandleFunc("/user/update", c.UpdateUser).Methods("POST")
-	a.Router.HandleFunc("/user/delete", c.DeleteUser).Methods("POST")
+	a.Router.HandleFunc("/users/get", c.GetUsers).Methods("GET", "OPTIONS")
+	a.Router.HandleFunc("/users/create", c.CreateUser).Methods("POST", "OPTIONS")
+	a.Router.HandleFunc("/user/get", c.GetUser).Methods("POST", "OPTIONS")
+	a.Router.HandleFunc("/user/update", c.UpdateUser).Methods("POST", "OPTIONS")
+	a.Router.HandleFunc("/user/delete", c.DeleteUser).Methods("POST", "OPTIONS")
 }
 
 func initTodoListController(a *App) {
 	b := &controllers.Controller{Db: a.Db}
 	c := &todolistmodule.TodoListController{Base: b}
 
-	a.Router.HandleFunc("/lists/get", c.GetLists).Methods("GET")
-	a.Router.HandleFunc("/lists/create", c.CreateTodoList).Methods("POST")
-	a.Router.HandleFunc("/list/get", c.GetList).Methods("POST")
-	a.Router.HandleFunc("/list/update", c.UpdateTodoList).Methods("POST")
-	a.Router.HandleFunc("/list/delete", c.DeleteTodoList).Methods("POST")
+	a.Router.HandleFunc("/lists/get", c.GetLists).Methods("GET", "OPTIONS")
+	a.Router.HandleFunc("/lists/create", c.CreateTodoList).Methods("POST", "OPTIONS")
+	a.Router.HandleFunc("/list/get", c.GetList).Methods("POST", "OPTIONS")
+	a.Router.HandleFunc("/list/update", c.UpdateTodoList).Methods("POST", "OPTIONS")
+	a.Router.HandleFunc("/list/delete", c.DeleteTodoList).Methods("POST", "OPTIONS")
 }
 
 func initTodoItemController(a *App) {
 	b := &controllers.Controller{Db: a.Db}
 	c := &todoitemmodule.TodoItemController{Base: b}
 
-	a.Router.HandleFunc("/items/get", c.GetTodoItems).Methods("GET")
-	a.Router.HandleFunc("/items/create", c.CreateTodoItem).Methods("POST")
-	a.Router.HandleFunc("/item/get", c.GetTodoItem).Methods("POST")
-	a.Router.HandleFunc("/item/update", c.UpdateTodoItem).Methods("POST")
-	a.Router.HandleFunc("/item/delete", c.DeleteTodoItem).Methods("POST")
+	a.Router.HandleFunc("/items/get", c.GetTodoItems).Methods("GET", "OPTIONS")
+	a.Router.HandleFunc("/items/create", c.CreateTodoItem).Methods("POST", "OPTIONS")
+	a.Router.HandleFunc("/item/get", c.GetTodoItem).Methods("POST", "OPTIONS")
+	a.Router.HandleFunc("/item/update", c.UpdateTodoItem).Methods("POST", "OPTIONS")
+	a.Router.HandleFunc("/item/delete", c.DeleteTodoItem).Methods("POST", "OPTIONS")
+}
+
+// RouterWithCORS : To prevent getting CORS errors from Angular UI
+type RouterWithCORS struct {
+	r *mux.Router
 }
 
 // Serve : Runs web server
 func (a *App) Serve() {
-	http.ListenAndServe(":8080", a.Router)
+	http.ListenAndServe(":8080", &RouterWithCORS{a.Router})
+}
+
+// ServeHTTP : A middleware to add necessary headers in order not to get CORS error
+func (s *RouterWithCORS) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if origin := r.Header.Get("Origin"); origin != "" {
+		// w.Header().Set("Access-Control-Allow-Origin", origin)
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers",
+			"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	}
+
+	// Stop here for a Preflighted OPTIONS request.
+	if r.Method == "OPTIONS" {
+		return
+	}
+
+	// Lets Gorilla work
+	s.r.ServeHTTP(w, r)
 }
